1e73f6bb80b13ef8b7782491fd10d39e0ffa78ae
ManueDiaz <md307145@students.davenport.k12.ia.us>
2019-05-28 12:48:46 -0500

    update

--- src/ThirdfloorM.java
+++ src/ThirdfloorM.java
@@ -35,8 +35,16 @@ public class ThirdfloorM {
            Escape.displaydeath();
         }
         else if(coise==2){
-            System.out.println("you looked up and there are cameras watching you. He comes out of his room. 1) Hide \n" +
-                    " behind the couch. 2) hide int the living room closet.");
+            System.out.println("you looked up and there are cameras watching you. He comes out of his room.");
+             System.out.println( "1) Hide behind the couch. 2) hide int the living room closet.");
+            System.out.println("                                ___________________                               ");
+            System.out.println("       _______________          |        |        |                 ");
+            System.out.println("   ___(_______________)__       |        |        |      ");
+            System.out.println("   |  |_______________|  |      |        |        |  ");
+            System.out.println("   |  (_______________)  |      |     [] | []     |         ");
+            System.out.println("   |  |_______________|  |      |        |        |             ");
+            System.out.println("   |                     |      |        |        |          ");
+            System.out.println("   |_____________________|      |________|________|           ");
             choise3=k.nextInt();
             choise3(choise3);
         }
@@ -46,7 +54,7 @@ public class ThirdfloorM {
             System.out.println("Did you really that would work.  ");
             Escape.displaydeath();
         } else if (choise3 == 2) {
-            System.out.println(" He could find you and goes back to the room next to the stairs.");
+            System.out.println(" He couldn't find you and goes back to the room next to the stairs.");
             backtoroom();
         }
     }
@@ -58,15 +66,7 @@ public class ThirdfloorM {
             if(s==1){
                 System.out.println("You made it past the door and manage to lock the kidnapper in the room.");
                 System.out.println("As you go for the Stair a second person comes from the stairs and shoots you.");
-                    System.out.println("            ___                   ");
-                    System.out.println("         __|- |              ");
-                    System.out.println("         |____|              ");
-                    System.out.println("      ___/    |___/ /            ");
-                    System.out.println("     / ___      ___/            ");
-                    System.out.println("    / /  |     |                 ");
-                    System.out.println("         |_____|                 ");
-                    System.out.println("        / /  / /                 ");
-                    System.out.println("       /_/  /_/                  ");
+                   shoot();
 
                     System.out.println("You died, try again");
                 System.out.print("try again");
@@ -75,6 +75,20 @@ public class ThirdfloorM {
             else
                 System.out.println("He comes out of his room and heads to the living room closet. he has a shotgun. and\n" +
                         "fires it at the living room closet.Try again");
+            shoot();
+        }
+        public static void shoot(){
+            System.out.println("            ___                   ");
+            System.out.println("         __|- |              ");
+            System.out.println("         |____|              ");
+            System.out.println("      ___/    |___/ /            ");
+            System.out.println("     / ___      ___/            ");
+            System.out.println("    / /  |     |                 ");
+            System.out.println("         |_____|                 ");
+            System.out.println("        / /  / /                 ");
+            System.out.println("       /_/  /_/                  ");
+
+
         }
 
     }
